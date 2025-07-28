45a572bda62d51684e29854e7dfeb23bd3604a23
Jonah-Schwartz521 <josc6888@colorado.edu>
2025-07-28 14:40:47 -0500

    Create base modeling CTE with clean and relevant features

---
+++ sparcs-sql-analysis/sql/04_feature_selection_and_cleaning.sql
@@ -0,0 +1,23 @@
+
+WITH modeling_cte AS (
+    SELECT
+        gender,
+        age_group,
+        length_of_stay::int AS los,
+        apr_severity_of_illness_description AS severity,
+        ccs_diagnosis_description,
+        ccs_procedure_description,
+        hospital_county,
+        payment_typology_1 AS payment_type,
+        type_of_admission,
+        total_charges 
+    FROM
+        modeling_table
+    WHERE 
+        total_charges IS NOT NULL
+        AND length_of_stay ~ '^\d+$'
+        AND gender IN ('M', 'F')
+)
+SELECT * 
+FROM modeling_cte
+LIMIT 50;
